0a987a9eabb1aa567ab02fb27fdee7825ff89f27
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-06-26 22:53:09 +0200

    task2

--- js/task-2.js
+++ js/task-2.js
@@ -26,3 +26,29 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+
+// Напиши скрипт для створення галереї зображень на підставі масиву даних.
+//  HTML містить список ul.gallery.
+// <ul class="gallery"></ul>
+
+// Використовуй масив об'єктів images для створення елементів <img>,
+// вкладених в <li>. Для створення розмітки використовуй шаблонні рядки
+// і метод insertAdjacentHTML().
+
+// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
+
+
+const galleryRef = document.querySelector(".gallery");
+
+// Function for create of markup (string) for 1 item (li) with img
+const imageMarkup = (image) => {
+  return `<li class="gallery-item"><img class="gallery-img" src="${image.url}" alt="${image.alt}" width="360" ></li>`;
+};
+
+// Create of markup (string) for all items
+const imagesMarkup = images.map(imageMarkup).join("");
+console.log(imagesMarkup);
+
+// Insert in DOM
+galleryRef.insertAdjacentHTML("beforeend", imagesMarkup);
