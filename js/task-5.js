ae85dda23db4a042063d697e12b9dc3732c7c4fa
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-06-27 20:28:22 +0200

    added task-5, moved getRandomHexColor to separate file

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,25 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+import getRandomHexColor from "./getRandomHexColor.js";
+
+/*
+Напиши скрипт, який змінює кольори фону елемента <body> 
+через інлайн-стиль по кліку на button.change-color і 
+виводить значення кольору в span.color.
+
+<div class="widget">
+  <p>Background color: <span class="color">-</span></p>
+  <button type="button" class="change-color">Change color</button>
+</div>
+
+Для генерування випадкового кольору використовуй функцію getRandomHexColor.*/
+
+const widget = document.querySelector(".widget");
+const changeColorBtn = widget.querySelector(".change-color");
+const colorSpan = widget.querySelector(".color");
+
+changeColorBtn.addEventListener("click", onChangeColor);
+
+function onChangeColor() {
+  const randomColor = getRandomHexColor();
+  document.querySelector("body").style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
 }
